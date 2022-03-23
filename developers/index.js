6f3504694621af869c94583fbf643a2438f65d0e
NyanKaungSet <96227457+NyanKaungSet@users.noreply.github.com>
2022-03-23 20:18:42 +0630

    Update index.js

--- developers/index.js
+++ developers/index.js
@@ -66,11 +66,3 @@ links.forEach(item=>{
 		el.scrollIntoView({behavior:"smooth",block:"start"});
 	});
 });
-
-document.querySelector('#code1').textContent=`
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-firestore.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
-<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-storage.js"></script>
-`;
